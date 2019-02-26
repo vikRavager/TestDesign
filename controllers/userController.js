d8b23a46a9d8917d7f4a36effc9156e768858c2a
vickypinak <pinakbagchi.115@gmail.com>
2019-02-27 01:51:44 +0530

    admin registration created.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res){
+        res.render('index');
+});
+
+router.get('/test1',function(req,res){
         res.render('test1');
+});
+
+router.get('/admin-registration',function(req,res){
+        res.render('adminRegistration');
 });
 
 module.exports = router;
